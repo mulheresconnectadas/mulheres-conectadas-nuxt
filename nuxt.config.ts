6024f79bde91a4cc7afa0f8e2f40904cbd9c3d7a
Castelo <jpcbm@ic.ufal.br>
2025-04-24 22:48:35 -0300

    Atualiza a configuração do Nuxt, adicionando um CSS global e corrigindo a formatação das strings nos módulos. Inclui também a adição de uma nova linha ao final do arquivo.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
+  // módulos instalados
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/ui",
+  ],
+
+  // seu CSS global
+  css: ["~/assets/css/main.css"],
+});
