4bf166fc9ade5332bb296ff009dedf334f5f1677
Castelo <jpcbm@ic.ufal.br>
2025-05-01 16:05:21 -0300

    Atualiza o componente AppHeader para utilizar o NuxtLink em vez de divs para os links de navegação, melhorando a acessibilidade e a semântica do código. Remove comentários desnecessários e ajusta a formatação dos links.

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,52 +1,63 @@
 <template>
   <header class="bg-gradient-to-r from-[#6C4BB2] to-[#EE3792] shadow-md">
     <div class="mx-auto px-20 py-6 flex justify-between items-center">
-      <div class="flex items-center space-x-4">
+      <NuxtLink
+        to="/"
+        class="flex items-center space-x-4"
+        active-class="border-none"
+      >
         <img src="/logo.png" alt="Mulheres Conectadas" class="w-52" />
-      </div>
+      </NuxtLink>
       <nav class="flex items-center space-x-8">
         <NuxtLink
           to="/"
           class="text-white hover:text-gray-200 pb-1"
           active-class="router-link-active"
           exact
-          >Início</NuxtLink
         >
-        <NuxtLink
+          Início
+        </NuxtLink>
+        <!-- <NuxtLink
           to="/institucional"
           class="text-white hover:text-gray-200 pb-1"
           active-class="router-link-active"
-          >Institucional</NuxtLink
         >
+          Institucional
+        </NuxtLink>
         <NuxtLink
           to="/solucoes"
           class="text-white hover:text-gray-200 pb-1"
           active-class="router-link-active"
-          >Soluções</NuxtLink
         >
+          Soluções
+        </NuxtLink>
         <NuxtLink
           to="/noticias"
           class="text-white hover:text-gray-200 pb-1"
           active-class="router-link-active"
-          >Notícias</NuxtLink
         >
+          Notícias
+        </NuxtLink>
         <NuxtLink
           to="/mapa"
           class="text-white hover:text-gray-200 pb-1"
           active-class="router-link-active"
-          >Mapa</NuxtLink
         >
+          Mapa
+        </NuxtLink>
         <NuxtLink
           to="/blog"
           class="text-white hover:text-gray-200 pb-1"
           active-class="router-link-active"
-          >Blog</NuxtLink
         >
+          Blog
+        </NuxtLink> -->
         <NuxtLink
           to="/inscreva-se"
           class="bg-[#4B2E83] text-white px-6 py-2 rounded-full hover:bg-opacity-90 hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-lg"
-          >Inscreva-se</NuxtLink
         >
+          Inscreva-se
+        </NuxtLink>
       </nav>
     </div>
   </header>
