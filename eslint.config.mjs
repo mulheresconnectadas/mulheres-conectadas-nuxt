e28f69776b7291ead5437756ea495a3d5439fb32
Castelo <jpcbm@ic.ufal.br>
2025-05-01 15:52:11 -0300

    Adiciona regras de formatação para fechamento automático de tags HTML no ESLint, melhorando a consistência do código.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,23 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
   // Your custom configs here
-)
+
+  {
+    rules: {
+      "vue/html-self-closing": [
+        "error",
+        {
+          html: {
+            void: "any", // permite <img/> ou <img>
+            normal: "never",
+            component: "always",
+          },
+          svg: "always",
+          math: "always",
+        },
+      ],
+    },
+  }
+);
