c9876a69c779e6c6705177b3378c5f65ed672458
Castelo <jpcbm@ic.ufal.br>
2025-05-01 15:41:04 -0300

    Adiciona o layout padrão com componentes AppHeader e AppFooter, estruturando a página com um template flexível.

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col h-screen relative">
+    <AppHeader class="sticky top-0 z-50" />
+    <slot />
+    <AppFooter class="mt-auto" />
+  </div>
+</template>
+
+<script setup></script>
