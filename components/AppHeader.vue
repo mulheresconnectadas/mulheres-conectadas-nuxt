0030ac78f0efa3f09305c309ed296dff25246728
Castelo <jpcbm@ic.ufal.br>
2025-05-01 15:54:00 -0300

    Atualiza o componente AppHeader para ajustar o tamanho da logo e adicionar a classe 'router-link-active' para estilização de links ativos. Também adiciona novas regras CSS para a classe 'router-link-active' no arquivo main.css.

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,31 +1,50 @@
 <template>
   <header class="bg-gradient-to-r from-[#6C4BB2] to-[#EE3792] shadow-md">
-    <div class="mx-auto px-4 py-6 flex justify-between items-center">
+    <div class="mx-auto px-20 py-6 flex justify-between items-center">
       <div class="flex items-center space-x-4">
-        <img src="/logo.png" alt="Mulheres Conectadas" class="w-60" />
+        <img src="/logo.png" alt="Mulheres Conectadas" class="w-52" />
       </div>
       <nav class="flex items-center space-x-8">
-        <NuxtLink to="/" class="text-white hover:text-gray-200"
+        <NuxtLink
+          to="/"
+          class="text-white hover:text-gray-200 pb-1"
+          active-class="router-link-active"
+          exact
           >Início</NuxtLink
         >
-        <NuxtLink to="/institucional" class="text-white hover:text-gray-200"
+        <NuxtLink
+          to="/institucional"
+          class="text-white hover:text-gray-200 pb-1"
+          active-class="router-link-active"
           >Institucional</NuxtLink
         >
-        <NuxtLink to="/solucoes" class="text-white hover:text-gray-200"
+        <NuxtLink
+          to="/solucoes"
+          class="text-white hover:text-gray-200 pb-1"
+          active-class="router-link-active"
           >Soluções</NuxtLink
         >
-        <NuxtLink to="/noticias" class="text-white hover:text-gray-200"
+        <NuxtLink
+          to="/noticias"
+          class="text-white hover:text-gray-200 pb-1"
+          active-class="router-link-active"
           >Notícias</NuxtLink
         >
-        <NuxtLink to="/mapa" class="text-white hover:text-gray-200"
+        <NuxtLink
+          to="/mapa"
+          class="text-white hover:text-gray-200 pb-1"
+          active-class="router-link-active"
           >Mapa</NuxtLink
         >
-        <NuxtLink to="/blog" class="text-white hover:text-gray-200"
+        <NuxtLink
+          to="/blog"
+          class="text-white hover:text-gray-200 pb-1"
+          active-class="router-link-active"
           >Blog</NuxtLink
         >
         <NuxtLink
           to="/inscreva-se"
-          class="bg-[#4B2E83] text-white px-6 py-2 rounded-full hover:bg-opacity-90 transition-colors"
+          class="bg-[#4B2E83] text-white px-6 py-2 rounded-full hover:bg-opacity-90 hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-lg"
           >Inscreva-se</NuxtLink
         >
       </nav>
