604711d6a7e1bb253faf8f3f0db9247567ea286c
Castelo <jpcbm@ic.ufal.br>
2025-05-08 00:33:46 -0300

    Atualiza o layout da página de inscrição, adicionando um fundo gradiente e uma imagem de título, além de integrar o componente MultiStepCard.

--- pages/inscricao/index.vue
+++ pages/inscricao/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h1>Inscrição</h1>
+  <div
+    class="bg-gradient-to-r from-[#2C144C] to-[#672FB2] flex flex-col space-y-10 py-10"
+  >
+    <img
+      src="/inscricao/title-inscricao.png"
+      alt="Inscrição"
+      class="w-full mt-10"
+    />
+    <MultiStepCard />
   </div>
 </template>
 
