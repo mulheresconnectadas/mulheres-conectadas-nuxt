ccbf79cb54ce8504edf2116a6212e8c57566c7a0
Castelo <jpcbm@ic.ufal.br>
2025-05-08 19:16:20 -0300

    Adiciona lógica para exibir mensagem de agradecimento ao final do fluxo de etapas no componente MultiStepCard, melhorando a experiência do usuário ao concluir o processo.

--- components/MultiStepCard/index.vue
+++ components/MultiStepCard/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="max-w-7xl mx-auto bg-white rounded-md shadow-lg overflow-hidden">
-    <div class="md:flex">
+    <div v-if="step < 5" class="md:flex">
       <LeftPanel :step="step" />
       <RightPanel :step="step" :form="form" @next="next" @prev="prev" />
+    </div>
+    <div v-else>
+      <Step5Thanks />
     </div>
   </div>
 </template>
@@ -10,7 +13,11 @@
 <script setup lang="ts">
 import LeftPanel from "./LeftPanel.vue";
 import RightPanel from "./RightPanel.vue";
+import Step5Thanks from "./LeftSteps/Step5Thanks.vue";
 import { useMultiStep } from "@/composables/useMultiStep";
+import { useForm } from "@/composables/useForm";
+const { step, next, prev } = useMultiStep();
 
-const { step, form, next, prev } = useMultiStep();
+const { form } = useForm();
+
 </script>
