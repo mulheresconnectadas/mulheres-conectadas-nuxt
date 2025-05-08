0e9ea4a9600dab0eb57e31b4aa87a778a6924d94
Castelo <jpcbm@ic.ufal.br>
2025-05-08 19:17:16 -0300

    Adiciona a interface IFormulario em um novo arquivo, definindo a estrutura do formulário com campos relevantes para coleta de dados, melhorando a tipagem e a organização do código.

---
+++ types/form.ts
@@ -0,0 +1,15 @@
+export interface IFormulario {
+  nome: string;
+  email: string;
+  data_nascimento: string;
+  genero: string;
+  etnia: string;
+  escolaridade: string;
+  contato: string;
+  situacao_trabalho: string;
+  rede_social: string;
+  cidade: string;
+  deseja_participar_presencial: string;
+  como_sobre_programa: string;
+  autorizacao_lgpd: string;
+} 
