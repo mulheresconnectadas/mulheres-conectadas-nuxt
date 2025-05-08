27af113c55849b8daae17dbab591f77a17d47aa7
Castelo <jpcbm@ic.ufal.br>
2025-05-08 01:17:40 -0300

    Adiciona transição de fade ao componente LeftPanel e refatora a lógica de seleção de componentes para utilizar props, melhorando a dinâmica de exibição entre os passos.

--- components/MultiStepCard/LeftPanel.vue
+++ components/MultiStepCard/LeftPanel.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="md:w-1/2 bg-[#6c4bb2] text-white p-8 flex flex-col rounded-l-md">
-    <component :is="leftComponent" class="flex-1" />
+    <transition name="fade" mode="out-in">
+      <component :is="leftComponent" class="flex-1" />
+    </transition>
   </div>
 </template>
 
@@ -9,13 +11,23 @@ import { computed } from "vue";
 import Step1Intro from "./LeftSteps/Step1Intro.vue";
 import Step2to4Visual from "./LeftSteps/Step2to4Visual.vue";
 import Step5Thanks from "./LeftSteps/Step5Thanks.vue";
-import { useMultiStep } from "@/composables/useMultiStep";
 
-const { step } = useMultiStep();
+const props = defineProps<{ step: number }>();
 
 const leftComponent = computed(() => {
-  if (step.value === 1) return Step1Intro;
-  if (step.value > 1 && step.value < 5) return Step2to4Visual;
+  if (props.step === 1) return Step1Intro;
+  if (props.step > 1 && props.step < 5) return Step2to4Visual;
   return Step5Thanks;
 });
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
