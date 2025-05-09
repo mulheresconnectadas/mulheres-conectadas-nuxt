1f9955329c21c0788e4f7cc7b4edb826efb9481f
Castelo <jpcbm@ic.ufal.br>
2025-05-08 21:50:26 -0300

    Atualiza o componente MultiStepCard para remover a cor de fundo do painel final e ajusta o estilo do componente Step5Thanks, aumentando o tamanho do título e simplificando o design.

--- components/MultiStepCard/LeftSteps/Step5Thanks.vue
+++ components/MultiStepCard/LeftSteps/Step5Thanks.vue
@@ -1,10 +1,9 @@
 <template>
   <div
-    class="p-12 bg-gradient-to-r from-purple-700 to-pink-500 rounded-br-[40px] rounded-tr-[40px] text-center"
+    class="p-12 bg-gradient-to-r from-purple-700 to-pink-500 rounded-[20px] text-center"
   >
-    <h2 class="text-4xl font-bold mb-4">AGRADECEMOS SUA INSCRIÇÃO</h2>
-    <p class="text-white">
-      Verifique seu e-mail para validar sua inscrição em nosso programa.
-    </p>
+    <h2 class="text-6xl font-bold mb-4 text-white">
+      AGRADECEMOS SUA INSCRIÇÃO!
+    </h2>
   </div>
 </template>
