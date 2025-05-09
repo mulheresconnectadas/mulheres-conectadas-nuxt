1071cb106a7e05ea813c25d1d4df0b08bc78f0d5
Castelo <jpcbm@ic.ufal.br>
2025-05-08 21:07:53 -0300

    Atualiza o componente Step4Form do MultiStepCard para incluir campos de formulário dentro de UFormField, adicionando tratamento de erros e implementando validação ao avançar para o próximo passo. Melhora a experiência do usuário ao garantir que todos os campos obrigatórios sejam preenchidos corretamente antes da submissão.

--- components/MultiStepCard/RightSteps/Step4Form.vue
+++ components/MultiStepCard/RightSteps/Step4Form.vue
@@ -1,33 +1,37 @@
 <template>
   <form class="space-y-4" @submit.prevent="emit('next')">
     <!-- Campos Etapa 4 -->
-    <div class="space-y-2">
-      <p class="text-[#2C144C] font-medium">
-        Como você ficou sabendo do Programa Mulheres Conectadas: Empoderamento
-        Digital no Semiárido?
-      </p>
+    <UFormField class="w-full" :error="error['como_soube_programa']">
+      <div class="space-y-2">
+        <p class="text-[#2C144C] font-medium">
+          Como você ficou sabendo do Programa Mulheres Conectadas: Empoderamento
+          Digital no Semiárido?
+        </p>
 
-      <URadioGroup
-        v-model="form.como_sobre_programa"
-        :items="sabendoOptions"
-        @update:model-value="
-          (val) => emit('update:form', 'como_sobre_programa', val)
-        "
-      />
-    </div>
+        <URadioGroup
+          v-model="form.como_soube_programa"
+          :items="sabendoOptions"
+          @update:model-value="
+            (val) => emit('update:form', 'como_soube_programa', val)
+          "
+        />
+      </div>
+    </UFormField>
     <!-- LGPD -->
-    <fieldset class="space-y-1">
-      <legend class="font-medium">
-        Você concorda com a utilização de seus dados (LGPD)?
-      </legend>
-      <URadioGroup
-        v-model="form.autorizacao_lgpd"
-        :items="lgpdOptions"
-        @update:model-value="
-          (val) => emit('update:form', 'autorizacao_lgpd', val)
-        "
-      />
-    </fieldset>
+    <UFormField class="w-full" :error="error['autorizacao_lgpd']">
+      <div class="space-y-2">
+        <p class="text-[#2C144C] font-medium">
+          Você concorda com a utilização de seus dados (LGPD)?
+        </p>
+        <URadioGroup
+          v-model="form.autorizacao_lgpd"
+          :items="lgpdOptions"
+          @update:model-value="
+            (val) => emit('update:form', 'autorizacao_lgpd', val)
+          "
+        />
+      </div>
+    </UFormField>
 
     <!-- Botões -->
     <div class="flex justify-evenly mt-6">
@@ -41,7 +45,8 @@
       <UButton
         type="button"
         class="w-full md:w-auto block shadow-md bg-pink-500 hover:bg-pink-600 text-white font-medium px-6 py-3 rounded-full transition hover:shadow-lg hover:scale-105 duration-300 cursor-pointer"
-        @click="submit"
+        :disabled="loading"
+        @click="next"
       >
         Finalizar Inscrição
       </UButton>
@@ -53,13 +58,34 @@
 import type { RadioGroupItem } from "@nuxt/ui";
 import type { IFormulario } from "@/types/form";
 import { useForm } from "@/composables/useForm";
-const { submit, form } = useForm();
+import { useValidateSteps } from "@/composables/useValidateSteps";
+const toast = useToast();
 
+const { submit, form } = useForm();
+const { validateStep4, error } = useValidateSteps();
+const loading = ref(false);
 const emit = defineEmits<{
   (e: "next" | "prev"): void;
   (e: "update:form", field: keyof IFormulario, value: string): void;
 }>();
 
+function next() {
+  const { valid } = validateStep4(form.value);
+
+  if (valid) {
+    loading.value = true;
+    submit().then(() => {
+      loading.value = false;
+      emit("next");
+    });
+  } else {
+    toast.add({
+      title: "Erro",
+      description: "Preencha todos os campos obrigatórios",
+      color: "neutral",
+    });
+  }
+}
 const lgpdOptions = ref<RadioGroupItem[]>([
   { label: "Sim", value: "sim" },
   { label: "Não", value: "nao" },
@@ -70,5 +96,6 @@ const sabendoOptions = ref<RadioGroupItem[]>([
   { label: "LinkedIn", value: "linkedin" },
   { label: "WhatsApp", value: "whatsapp" },
   { label: "TV", value: "tv" },
+  { label: "Outros", value: "outros" },
 ]);
 </script>
