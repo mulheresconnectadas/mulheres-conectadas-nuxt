ef97384e6f0559cc1487a169cd5b7f90d569c015
Castelo <jpcbm@ic.ufal.br>
2025-05-12 17:00:24 -0300

    Atualiza o componente Step5Thanks para melhorar a apresentação visual, alterando as cores do fundo e adicionando uma mensagem de confirmação sobre a validação do e-mail, aprimorando a experiência do usuário após a inscrição.

--- components/MultiStepCard/LeftSteps/Step5Thanks.vue
+++ components/MultiStepCard/LeftSteps/Step5Thanks.vue
@@ -1,9 +1,16 @@
 <template>
   <div
-    class="p-12 bg-gradient-to-r from-purple-700 to-pink-500 rounded-[20px] text-center"
+    class="p-12 bg-gradient-to-r from-[#672FB2] to-[#EE3792] rounded-[20px] text-center flex flex-col items-center shadow-inner"
   >
-    <h2 class="text-6xl font-bold mb-4 text-white">
+    <h2
+      class="text-6xl font-bold text-white border-b border-white max-w-4xl py-10"
+    >
       AGRADECEMOS SUA INSCRIÇÃO!
     </h2>
+    <div class="py-10">
+      <p class="text-white text-2xl">
+        Verifique seu e-mail para validar sua inscrição em nosso programa.
+      </p>
+    </div>
   </div>
 </template>
