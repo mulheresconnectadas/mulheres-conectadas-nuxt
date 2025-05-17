39fa9651c701d30f23cfff0d886e8a51d1cc9676
Castelo <jpcbm@ic.ufal.br>
2025-05-17 18:53:13 -0300

    Adiciona o componente LoggedLayout, que estrutura a interface de usuário para usuários autenticados, incluindo um cabeçalho e um espaço principal para conteúdo dinâmico.

---
+++ layouts/logged.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <AuthHeaderLogged />
+
+    <main class="flex-1 flex items-center justify-center bg-white px-4">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+// não precisa mudar nada aqui
+</script>
