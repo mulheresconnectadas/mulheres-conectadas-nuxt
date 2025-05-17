33eddf6b895fa3b81c5343952026982487aa697f
Castelo <jpcbm@ic.ufal.br>
2025-05-17 18:52:29 -0300

    Adiciona o componente HeaderLogged, que implementa um cabeçalho com logo e navegação para as páginas de Dashboard e Perfil, melhorando a estrutura visual da interface de autenticação.

---
+++ components/auth/HeaderLogged.vue
@@ -0,0 +1,37 @@
+<script setup></script>
+
+<template>
+  <header
+    class="bg-gradient-to-r from-[#6C4BB2] to-[#EE3792] shadow-md transform py-2"
+  >
+    <div
+      class="mx-auto px-4 flex justify-between items-center transition-all duration-300 max-w-7xl"
+    >
+      <NuxtLink
+        to="/"
+        class="flex items-center space-x-4"
+        active-class="border-none"
+      >
+        <img src="/logo.png" alt="Mulheres Conectadas" class="w-52 logo" />
+      </NuxtLink>
+      <nav class="flex items-center space-x-8">
+        <NuxtLink
+          to="/dashboard"
+          class="text-white hover:text-gray-200 pb-1 nav-link"
+          active-class="router-link-active"
+          exact
+        >
+          Dashboard
+        </NuxtLink>
+        <NuxtLink
+          to="/"
+          class="text-white hover:text-gray-200 pb-1 nav-link"
+          active-class="router-link-active"
+          exact
+        >
+          Perfil
+        </NuxtLink>
+      </nav>
+    </div>
+  </header>
+</template>
