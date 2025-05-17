2f1b599fde78223fecb1df0bd8d3ae456c72991b
Castelo <jpcbm@ic.ufal.br>
2025-05-17 16:27:36 -0300

    Atualiza o layout de autenticação para incluir um contêiner principal que centraliza o conteúdo, melhorando a apresentação visual e a experiência do usuário.

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <AuthHeader />
-    <slot />
+
+    <main class="flex-1 flex items-center justify-center bg-white px-4">
+      <slot />
+    </main>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+// não precisa mudar nada aqui
+</script>
