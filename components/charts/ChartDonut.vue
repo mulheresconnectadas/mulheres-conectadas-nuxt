78ee531eb89700d102af32e5f69a14e14e4301c3
Castelo <jpcbm@ic.ufal.br>
2025-05-17 18:52:38 -0300

    Adiciona o componente ChartDonut, que implementa um gráfico de pizza utilizando as bibliotecas vue-chartjs e chart.js, permitindo a visualização de dados de escolaridade de forma interativa e responsiva.

---
+++ components/charts/ChartDonut.vue
@@ -0,0 +1,41 @@
+<template>
+  <Doughnut :data="chartData" :options="options" />
+</template>
+
+<script setup lang="ts">
+import { Doughnut } from "vue-chartjs";
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement } from "chart.js";
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement);
+
+const props = defineProps<{
+  labels: string[];
+  data: number[];
+}>();
+
+const chartData = {
+  labels: props.labels,
+  datasets: [
+    {
+      label: "Escolaridade",
+      data: props.data,
+      backgroundColor: [
+        "#d946ef",
+        "#38bdf8",
+        "#a3e635",
+        "#fb923c",
+        "#6366f1",
+        "#f43f5e",
+      ],
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: { position: "right" as const },
+    title: { display: false },
+  },
+};
+</script>
