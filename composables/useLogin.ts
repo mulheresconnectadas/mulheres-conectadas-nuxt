47505cbd2d5f835d65e444452e56b50beefb5cf5
Castelo <jpcbm@ic.ufal.br>
2025-05-17 16:55:58 -0300

    Adiciona o composable useLogin, que gerencia o estado do formulário de login e implementa a função de submissão para autenticação, integrando a chamada à API de login.

---
+++ composables/useLogin.ts
@@ -0,0 +1,38 @@
+import type { ILogin } from "@/types/form";
+export function useLogin() {
+  const form = useState<ILogin>("form", () => ({
+    email: "",
+    senha: "",
+  }));
+
+  // composable.ts
+  async function submit(email: string, senha: string) {
+    const response = await fetch(
+      "https://back-mulheres-conectadas.vercel.app/participantes/admin/login",
+      {
+        method: "POST",
+        body: JSON.stringify({ email, senha }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (!response.ok) {
+      let errorData;
+      try {
+        errorData = await response.json();
+      } catch {
+        errorData = { message: "Erro desconhecido." };
+      }
+      console.log("ERROR DATA", errorData);
+
+      throw new Error(
+        errorData.detail[0].msg || errorData.detail || `Erro ${response.status}`
+      );
+    }
+
+    return await response.json();
+  }
+
+  return { submit, form };
+}
