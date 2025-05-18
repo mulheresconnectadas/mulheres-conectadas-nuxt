e464229871cc23b24e872a2685db332d48f71bea
Castelo <jpcbm@ic.ufal.br>
2025-05-18 15:54:34 -0300

    Atualiza o componente Step5Thanks.vue para melhorar o layout e a formatação do texto, ajustando margens e tamanhos de fonte.

--- components/MultiStepCard/LeftSteps/Step5Thanks.vue
+++ components/MultiStepCard/LeftSteps/Step5Thanks.vue
@@ -1,15 +1,18 @@
 <template>
   <div
-    class="p-12 bg-gradient-to-r from-[#672FB2] to-[#EE3792] rounded-[20px] text-center flex flex-col items-center shadow-inner"
+    class="px-12 py-8 bg-gradient-to-r from-[#672FB2] to-[#EE3792] rounded-[20px] text-center flex flex-col items-center shadow-inner"
   >
     <h2
-      class="text-6xl font-bold text-white border-b border-white max-w-4xl py-10"
+      class="text-6xl font-extrabold text-white border-b border-white max-w-4xl py-5"
     >
       AGRADECEMOS SUA INSCRIÇÃO!
     </h2>
-    <div class="py-10">
-      <p class="text-white text-2xl">
+    <div class="py-5">
+      <p class="text-white text-lg">
         Verifique seu e-mail para validar sua inscrição em nosso programa.
+        <br />
+        Caso não encontre, lembre-se de checar sua caixa de spam ou lixo
+        eletrônico.
       </p>
     </div>
   </div>
