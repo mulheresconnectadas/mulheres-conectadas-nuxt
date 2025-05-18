6947f5ca6471d7e5d3cfc9fabef9618c3402dac3
Castelo <jpcbm@ic.ufal.br>
2025-05-18 20:00:09 -0300

    Atualiza o componente MultiStepCard.vue, melhorando a responsividade e a apresentação visual com ajustes nas classes de estilo, além de reorganizar a estrutura do template para melhor adaptação em diferentes tamanhos de tela.

--- components/MultiStepCard/index.vue
+++ components/MultiStepCard/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="max-w-7xl w-full mx-auto overflow-hidden">
-    <div v-if="step < 5" class="md:flex rounded-md shadow-lg">
+  <div class="max-w-7xl w-full mx-auto px-4 sm:px-6">
+    <div
+      v-if="step < 5"
+      class="flex flex-col md:flex-row rounded-md shadow-lg overflow-hidden"
+    >
       <LeftPanel :step="step" />
       <RightPanel :step="step" :form="form" @next="next" @prev="prev" />
     </div>
-    <div v-else class="w-full max-w-4xl mx-auto">
+
+    <div v-else class="w-full max-w-4xl mx-auto py-8 px-4 sm:px-6">
       <Step5Thanks />
     </div>
   </div>
@@ -16,7 +20,7 @@ import RightPanel from "./RightPanel.vue";
 import Step5Thanks from "./LeftSteps/Step5Thanks.vue";
 import { useMultiStep } from "@/composables/useMultiStep";
 import { useForm } from "@/composables/useForm";
-const { step, next, prev } = useMultiStep();
 
+const { step, next, prev } = useMultiStep();
 const { form } = useForm();
 </script>
